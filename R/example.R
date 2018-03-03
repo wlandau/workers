ac4b44de9ad7b8f1783c59cb82e7a867b3bc0d79
wlandau <will.landau@gmail.com>
2018-03-03 13:04:08 -0500

    Example jobs and sched

--- R/example.R
+++ R/example.R
@@ -1,6 +1,16 @@
 example_args <- function(){
-  config <- drake::load_basic_example(envir = globalenv(), cache = storr::storr_environment())
-  graph <- drake:::targets_graph(config)
-  jobs <- config$plan
-  list(graph = graph, jobs = jobs)
+  config <- drake::load_basic_example(
+    envir = new.env(),
+    cache = storr::storr_environment(),
+    verbose = FALSE
+  )
+  unlink(c("report.md", "report.Rmd"))
+  schedule <- drake:::targets_graph(config)
+  cmd <- config$plan$command[-1]
+  cmd[1] <- "data.frame(x = rnorm(48), y = rnorm(48))"
+  cmd[2] <- "data.frame(x = rnorm(64), y = rnorm(64))"
+  jobs <- as.list(parse(text = cmd, keep.source = FALSE))
+  names(jobs) <- config$plan$target[-1]
+  jobs <- list2env(jobs)
+  list(schedule = schedule, jobs = jobs)
 }
