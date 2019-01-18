9c3f57195a04857a2844114f2a5c424757bc49f8
wlandau <will.landau@gmail.com>
2019-01-18 10:28:06 -0500

    Test on empty graph

--- tests/testthat/test-scheduler.R
+++ tests/testthat/test-scheduler.R
@@ -1,23 +1,12 @@
 context("test-scheduler")
 
 test_that("empty graph", {
-  edges <- data.frame(
-    from = c("a", "a", "b", "c"),
-    to = c("b", "c", "d", "d"),
-    stringsAsFactors = FALSE
-  )
-  graph <- igraph::graph_from_data_frame(edges)
-
+  graph <- igraph::make_empty_graph()
 })
 
 test_that("empty graph", {
-  edges <- data.frame(
-    from = c("a", "a", "b", "c"),
-    to = c("b", "c", "d", "d"),
-    stringsAsFactors = FALSE
-  )
-  graph <- igraph::graph_from_data_frame(edges)
-
+  graph <- igraph::make_empty_graph() + igraph::vertices("x")
+  schedule(graph)
 })
 
 test_that("uncomplicated graph", {
