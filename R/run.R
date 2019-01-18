f283d7b47fe9200def6b92ba7f5e43c2cd6d0a8f
Kirill Müller <krlmlr+r@mailbox.org>
2019-01-18 09:26:29 -0600

    Initial implementation, disregarding topological order

--- R/run.R
+++ R/run.R
@@ -1,5 +1,7 @@
 #' Run jobs in topological order
 #' @export
 schedule <- function(graph) {
-  stop("NYI")
+  code <- igraph::vertex.attributes(graph)$code
+  map(code, rlang::eval_tidy)
+  invisible()
 }
