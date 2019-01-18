5f45f6f7c25cdbb689cb504252c4ee73acc907da
wlandau <will.landau@gmail.com>
2019-01-18 17:51:57 -0500

    Sketch scheduler

--- R/scheduler.R
+++ R/scheduler.R
@@ -3,10 +3,27 @@
 #' @export
 schedule <- function(graph) {
   code <- V(graph)$code
+  queue <- new_priority_queue(graph)
+  workers <- new.env(parent = emptyenv())
+  while (work_remains(queue, workers)) {
+    launch_code(queue, workers)
+    eapply(workers, resolve_worker)
+    Sys.sleep(0.1) # Use backoff function
+  }
+} # rlang::invoke(code)
 
-  topo_order <- as.integer(topo_sort(graph))
-  code <- code[topo_order]
+work_remains <- function(queue, workers) {
+  !queue$empty() || length(names(workers))
+}
 
-  map(code, rlang::invoke)
-  invisible()
+launch_code <- function(queue, workers) {
+  id <- queue$pop0()
+  if (!length(id)) {
+    return()
+  }
+  workers[[id]] <- rlang::invoke(code)
+}
+
+resolve_worker <- function(worker) {
+  browser()
 }
