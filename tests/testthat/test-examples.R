663090a6e8ce0498ea077cc9dba97e5f9fd8dbdf
wlandau <will.landau@gmail.com>
2019-01-18 10:02:05 -0500

    Clear out old functionality

--- tests/testthat/test-examples.R
+++
@@ -1,8 +0,0 @@
-context("examples")
-
-test_that("example inputs to hire()", {
-  expect_is(example_schedule(format = "tibble"), "tbl")
-  expect_is(example_schedule(format = "igraph"), "igraph")
-  expect_is(example_workload(format = "tibble"), "tbl")
-  expect_is(example_workload(format = "environment"), "environment")
-})
