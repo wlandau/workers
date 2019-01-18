7c28e7c74edf5270dadd46bd6e6ce744495868ce
Kirill Müller <krlmlr+r@mailbox.org>
2019-01-18 09:39:37 -0600

    Align

--- R/run.R
+++
@@ -1,9 +0,0 @@
-#' Run jobs in topological order
-#' @export
-schedule <- function(graph, code) {
-  node_labels <- igraph::vertex.attributes(graph)$name
-  code <- code[node_labels]
-  browser()
-  map(code, rlang::invoke)
-  invisible()
-}
