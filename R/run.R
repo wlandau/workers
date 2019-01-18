54ee223ac34eaa745a9310ff65a79dfb747b7c6d
Kirill Müller <krlmlr+r@mailbox.org>
2019-01-18 09:38:58 -0600

    Tests show warnings, first success

--- R/run.R
+++ R/run.R
@@ -1,7 +1,9 @@
 #' Run jobs in topological order
 #' @export
-schedule <- function(graph) {
-  code <- igraph::vertex.attributes(graph)$code
-  map(code, rlang::call2)
+schedule <- function(graph, code) {
+  node_labels <- igraph::vertex.attributes(graph)$name
+  code <- code[node_labels]
+  browser()
+  map(code, rlang::invoke)
   invisible()
 }
--- tests/testthat/test-scheduler.R
+++ tests/testthat/test-scheduler.R
@@ -6,21 +6,18 @@ test_that("empty graph", {
 
 test_that("empty graph", {
   graph <- igraph::make_empty_graph() + igraph::vertices("x")
-  schedule(graph)
+  schedule(graph, list(x = function() warning()))
 })
 
 test_that("uncomplicated graph", {
-  vertices <- tibble::tibble(
-    name = letters[1:4],
-    code = map(letters[1:4], function(x) function() warning(x))
-  )
+  code <- map(rlang::set_names(letters[1:4]), function(x) function() warning(x))
 
   edges <- data.frame(
     from = c("a", "a", "b", "c"),
     to = c("b", "c", "d", "d"),
     stringsAsFactors = FALSE
   )
-  graph <- igraph::graph_from_data_frame(edges, vertices = vertices)
+  graph <- igraph::graph_from_data_frame(edges)
 
-  schedule(graph)
+  schedule(graph, code)
 })
