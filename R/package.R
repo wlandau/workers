aea5367e511be6e37165843772a9458d94fa5783
Kirill Müller <krlmlr+r@mailbox.org>
2019-01-18 23:52:50 -0600

    Need to import so that S3 registration works

--- R/package.R
+++ R/package.R
@@ -5,7 +5,7 @@
 #' @author William Michael Landau \email{will.landau@@gmail.com}
 #' @references <https://github.com/wlandau/workers>
 #' @rawNamespace import(igraph, except = "is_named")
-#' @importFrom future future resolved
+#' @importFrom future future resolved resolve run
 #' @importFrom methods new
 #' @importFrom rlang invoke is_logical is_true set_names
 NULL
